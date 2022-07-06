96e97ad526f20c215d9e6eb0dbe88bc4fcacc212
Mercy <mercy.akede@gmail.com>
2022-07-06 10:19:06 -0400

    Updated CRUD UI

--- client/script.js
+++ client/script.js
@@ -1,78 +1,66 @@
-function formToObject(htmlFormElement) {
-  const formItem = new FormData(htmlFormElement).entries();
-  const formArray = Array.from(formItem);
-  const formObject = formArray.reduce((collection, item, index) => {
-    if (!collection[item[0]]) {
-      collection[item[0]] = item[1];
-    }
-    return collection;
-  }, {});
-  return formObject;
+let selectedRow = null;
+function onFormSubmit(e) {
+  event.preventDefault();
+  let formData = readFormData();
+  if (selectedRow === null) {
+    insertNewRecord(formData);
+  }
+  else {
+    updateRecord(formData);
+  }
+  resetForm();
 }
 
-async function mainEvent() { // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form'); // change this selector to match the id or classname of your actual form
+// Retrieve the data
+function readFormData() {
+  let formData = {};
+  formData['cerealName'] = document.getElementById('cerealName').value;
+  formData['hotCold'] = document.getElementById('hotCold').value;
+  formData['calories'] = document.getElementById('calories').value;
+  return formData;
+}
 
-  const selectPark = document.querySelector('.select_park');
+// Insert the data
+function insertNewRecord(data) {
+  let table = document.getElementById('storeList').getElementsByTagName('tbody')[0];
+  let newRow = table.insertRow(table.length);
+  let cell1 = newRow.insertCell(0);
+  cell1.innerHTML = data.cerealName;
+  let cell2 = newRow.insertCell(1);
+  cell2.innerHTML = data.hotCold;
+  let cell3 = newRow.insertCell(2);
+  cell3.innerHTML = data.calories;
+  let cell4 = newRow.insertCell(3);
+  cell4.innerHTML = `<button onClick='onEdit(this)'>Edit</button> <button onClick='onDelete(this)'>Delete</button>`;
+}
 
-  const parks = document.querySelector('#park');
-  const refresh = document.querySelector('.refresh_list');
+// Edit the data
+function onEdit(td) {
+  selectedRow = td.parentElement.parentElement;
+  document.getElementById('cerealName').value = selectedRow.cells[0].innerHTML;
+  document.getElementById('hotCold').value = selectedRow.cells[1].innerHTML;
+  document.getElementById('calories').value = selectedRow.cells[2].innerHTML;
+}
 
-  const map = initMap('map');
-  const retrievalVar = 'parks';
-  selectPark.style.display = 'none';
+function updateRecord(formData) {
+  selectedRow.cells[0].innerHTML = formData.cerealName;
+  selectedRow.cells[1].innerHTML = formData.hotCold;
+  selectedRow.cells[2].innerHTML = formData.calories;
+}
 
-  // refreshList(refresh, retrievalVar);
-
-  const parksapi = await fetch('/api/race/');
-  const parksjson = await parksapi.json();
-  const parksArray = parksjson.parks;
-  // const storedDataArray = JSON.parse(parksArray);
-  // console.log(parksArray);
-  // console.log(storedDataArray);
-
-  updateParks(parksArray);
-  if (parksArray?.length > 0) {
-    currentPark = parksArray[0];
-    // this statement is to prevent a race condition on data load
-    selectPark.style.display = 'block';
-
-    let currentArray = parksArray;
-    // inputListener(parks);
-    addMapMarkers(map, currentArray);
-    parks.addEventListener('change', async (event) => {
-      // console.log(event.target.value);
-      // console.log(parksArray);
-      matchingPark = findPark(event.target.value, parksArray);
-      // console.log(matchingPark.park_lat, matchingPark.park_long );
-      currentPark = matchingPark;
-      updateMap(map, matchingPark.park_lat, matchingPark.park_long);
-    });
-
-    form.addEventListener('submit', async (submitEvent) => {
-      // async has to be declared all the way to get an await
-      submitEvent.preventDefault(); // This prevents your page from refreshing!
-      // console.log('form submission'); // this is substituting for a "breakpoint"
-    });
-    selectPark.addEventListener('click', async (submitEvent) => {
-      // async has to be declared all the way to get an await
-      submitEvent.preventDefault(); // This prevents your page from refreshing!
-      // console.log('form submission'); // this is substituting for a "breakpoint"
-      console.log('park selected');
-      console.log(currentPark.park_id);
-      displayReviews(currentPark.park_id);
-    });
-    refresh.addEventListener('click', async (submitEvent) => {
-      // async has to be declared all the way to get an await
-      submitEvent.preventDefault(); // This prevents your page from refreshing!
-      // console.log('form submission'); // this is substituting for a "breakpoint"
-      currentArray = parksArray;
-      console.log(currentArray);
-      updateParks(currentArray);
-      addMapMarkers(map, currentArray);
-    });
+// Delete the data
+function onDelete(td) {
+  if (confirm('Do you want to delete this record?')) {
+    row = td.parentElement.parentElement;
+    document.getElementById('storeList').deleteRow(row.rowIndex);
+    resetForm();
   }
 }
 
-// this actually runs first! It's calling the function above
-document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+// Reset the data
+function resetForm () {
+  document.getElementById('cerealName').value = '';
+  document.getElementById('hotCold').value = '';
+  document.getElementById('calories').value = '';
+  selectedRow = null;
+}
